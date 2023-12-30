54500bd8f2c2528e0f91f85b45db6f57b91557c0
Devin <dcolwell634@gmail.com>
2023-12-30 07:39:19 -0500

    Add files via upload

---
+++ Fun With Delegates/Apple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fun_With_Delegates
+{
+	internal class Apple
+	{
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
